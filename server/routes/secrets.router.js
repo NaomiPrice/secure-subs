2ab393c2ddf6c0dc6673225f78f6cb3ea20b1b32
Naomi Price <naomi.price71@gmail.com>
2019-10-09 14:58:02 -0500

    server side security added

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    let queryText = `SELECT "content" 
+                     FROM "secret"
+                     WHERE "secrecy_level" < $1;`
+    pool.query(queryText, [req.user.clearance_level])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
